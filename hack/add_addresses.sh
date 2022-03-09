c6e107d21cdd0e3afca29d8d2a9de02d0949ac8c
Joshua Reed <rejoshed@amazon.com>
2022-03-09 10:56:23 -0800

    Minor cleanup.

--- hack/add_addresses.sh
+++ hack/add_addresses.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This is a very simple script that can be used to launch a load balancer and add addresses to it.
+
+# It does so by querying kubernetes and parsing the output via jq.
+
 while true; do
     ADDRESSES=$(kubectl get machine -o json | jq -r '.items[] | select(.metadata.labels."cluster.x-k8s.io/control-plane" != null) | .status | select(.addresses!=null) | .addresses[].address')
     if [[ $ADDRESSES != $OLD_ADDRESSES ]]; then
@@ -10,8 +14,8 @@ while true; do
             echo $ADDRESS
             sed -i.bak '/upstream kubeendpoints/a\'$'\n'$'\t''server '$ADDRESS':6443 max_fails=3 fail_timeout=10s;'$'\n' nginx.conf
         done
-        docker stop nginx-container || echo
-        docker rm nginx-container || echo
+        docker stop nginx-container &> /dev/null || echo
+        docker rm nginx-container &> /dev/null || echo
         docker run --name=nginx-container --rm -p 8082:8082 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf nginx &
     fi
     OLD_ADDRESSES=$ADDRESSES 
