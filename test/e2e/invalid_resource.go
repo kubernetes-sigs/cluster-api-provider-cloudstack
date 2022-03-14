63bab263a0beb82b4aabdfac4cdc306d48ca9d11
Wonkun Kim <wonkun@amazon.com>
2022-03-14 15:19:46 -0500

    fix invalid resource test to pass

--- test/e2e/invalid_resource.go
+++ test/e2e/invalid_resource.go
@@ -86,7 +86,7 @@ func InvalidResourceSpec(ctx context.Context, inputGetter func() CommonSpecInput
 	})
 
 	It("Should fail due to the compute resources are not sufficient for the specified offering [TC8]", func() {
-		testInvalidResource(ctx, input, "insufficient-compute-resources", "CloudStackMachine VM in error state.  Deleting associated Machine.")
+		testInvalidResource(ctx, input, "insufficient-compute-resources", "Unable to create a deployment for VM")
 	})
 
 	AfterEach(func() {
