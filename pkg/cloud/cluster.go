07236f3aac5ccd41cd5f73dfdcbd11929509a1fb
Wonkun Kim <wonkun@amazon.com>
2022-04-01 14:13:04 -0500

    Support domain path in domain: property

--- pkg/cloud/cluster.go
+++ pkg/cloud/cluster.go
@@ -23,6 +23,8 @@ import (
 	capiv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
+const domainPrefix = "ROOT/"
+
 type ClusterIface interface {
 	GetOrCreateCluster(*infrav1.CloudStackCluster) error
 	DisposeClusterResources(cluster *infrav1.CloudStackCluster) error
@@ -67,19 +69,8 @@ func (c *client) GetOrCreateCluster(csCluster *infrav1.CloudStackCluster) (retEr
 		csCluster.Status.FailureDomains[zone.ID] = capiv1.FailureDomainSpec{ControlPlane: true}
 	}
 
-	// If provided, translate Domain name to Domain ID.
-	if csCluster.Spec.Domain != "" {
-		p := c.cs.Domain.NewListDomainsParams()
-		p.SetListall(true)
-		p.SetName(csCluster.Spec.Domain)
-		resp, retErr := c.cs.Domain.ListDomains(p)
-		if retErr != nil {
-			return retErr
-		} else if resp.Count != 1 {
-			return errors.Errorf("expected 1 Domain with name %s, but got %d", csCluster.Spec.Domain, resp.Count)
-		} else {
-			csCluster.Status.DomainID = resp.Domains[0].Id
-		}
+	if retErr := c.ResolveDomainAndAccount(csCluster); retErr != nil {
+		return retErr
 	}
 
 	// Get current network statuses.
@@ -92,6 +83,44 @@ func (c *client) GetOrCreateCluster(csCluster *infrav1.CloudStackCluster) (retEr
 		return c.GetOrCreateIsolatedNetwork(csCluster)
 	}
 
+	return nil
+}
+
+func (c *client) ResolveDomainAndAccount(csCluster *infrav1.CloudStackCluster) error {
+	if (csCluster.Spec.Domain != "" && csCluster.Spec.Account == "") ||
+		(csCluster.Spec.Domain == "" && csCluster.Spec.Account != "") {
+		return errors.Errorf("Both domain and account must be specified or none of them must be specified")
+	}
+
+	if csCluster.Spec.Domain != "" && csCluster.Spec.Account != "" {
+		p := c.cs.Domain.NewListDomainsParams()
+		p.SetListall(true)
+		resp, retErr := c.cs.Domain.ListDomains(p)
+		if retErr != nil {
+			return retErr
+		} else {
+			for _, domain := range resp.Domains {
+				if domain.Path == domainPrefix+csCluster.Spec.Domain {
+					csCluster.Status.DomainID = domain.Id
+					break
+				}
+			}
+		}
+		if csCluster.Status.DomainID == "" {
+			return errors.Errorf("domain not found for domain path %s", csCluster.Spec.Domain)
+		}
+
+		listAccountParams := c.cs.Account.NewListAccountsParams()
+		listAccountParams.SetDomainid(csCluster.Status.DomainID)
+		listAccountParams.SetName(csCluster.Spec.Account)
+		listAccountResp, retErr := c.cs.Account.ListAccounts(listAccountParams)
+		if retErr != nil {
+			return retErr
+		} else if listAccountResp.Count != 1 {
+			return errors.Errorf("expected 1 Account with account name %s in domain ID %s, but got %d",
+				csCluster.Spec.Account, csCluster.Status.DomainID, resp.Count)
+		}
+	}
 	return nil
 }
 
