88f4c8c53f7d4ab80b078387e95f9f36fc47eeab
Joshua Reed <rejoshed@amazon.com>
2022-03-31 22:19:18 -0700

    Such awesome, much wow

--- controllers/cloudstackcluster_controller.go
+++ controllers/cloudstackcluster_controller.go
@@ -18,16 +18,16 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/go-logr/logr"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	controlplanev1 "sigs.k8s.io/cluster-api/controlplane/kubeadm/api/v1beta1"
-
-	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
+	"github.com/prometheus/common/log"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-
+	controlplanev1 "sigs.k8s.io/cluster-api/controlplane/kubeadm/api/v1beta1"
 	"sigs.k8s.io/cluster-api/util"
 
 	"sigs.k8s.io/cluster-api/util/annotations"
@@ -46,12 +46,19 @@ import (
 	capiv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
+type ClusterContext struct {
+	CAPICluster *capiv1.Cluster
+	CSCluster   *infrav1.CloudStackCluster
+	Zones       *infrav1.CloudStackZoneList
+}
+
 // CloudStackClusterReconciler reconciles a CloudStackCluster object.
 type CloudStackClusterReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
 	CS     cloud.Client
+	CC     *ClusterContext
 }
 
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=cloudstackclusters,verbs=get;list;watch;create;update;patch;delete
@@ -68,101 +75,141 @@ type CloudStackClusterReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+
+type CloudStackClusterReconcilerFunc func(context.Context, ctrl.Request) (ctrl.Result, error)
+
 func (r *CloudStackClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (retRes ctrl.Result, retErr error) {
 	log := r.Log.WithValues("cluster", req.Name, "namespace", req.Namespace)
+	fmt.Println(req.NamespacedName)
 	log.V(1).Info("Reconcile CloudStackCluster")
 
-	// Get CloudStack cluster.
-	csCluster := &infrav1.CloudStackCluster{}
-	if err := r.Client.Get(ctx, req.NamespacedName, csCluster); err != nil {
-		if client.IgnoreNotFound(err) == nil {
-			log.Info("Cluster not found.")
-		}
-		return ctrl.Result{}, client.IgnoreNotFound(err)
-	}
+	return r.RunWith(ctx, req,
+		r.fetchRelatedCRDs,
+		r.checkIfPaused,
+		r.reconcile,
+		r.reconcileDelete,
+		r.patchCSCluster,
+	)
 
-	// Get CAPI cluster.
-	capiCluster, err := util.GetOwnerCluster(ctx, r.Client, csCluster.ObjectMeta)
-	if err != nil {
-		return reconcile.Result{}, err
-	} else if capiCluster == nil {
-		log.Info("Waiting for CAPI Cluster controller to set owner reference on CloudStack cluster.")
-		return reconcile.Result{}, nil
-	}
+	// if len(r.CC.CSCluster.Spec.Zones) != len(r.CC.Zones.Items) {
+	// 	return reconcile.Result{}, errors.New("did not find all zones")
+	// }
+}
 
-	for _, zone := range csCluster.Status.Zones {
-		r.generateZone(ctx, csCluster, zone)
-	}
-
-	// Check the cluster is not paused.
-	if annotations.IsPaused(capiCluster, csCluster) {
+// checkIfPaused returns with reque later set if paused.
+func (r *CloudStackClusterReconciler) checkIfPaused(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	if annotations.IsPaused(r.CC.CAPICluster, r.CC.CSCluster) {
 		log.Info("Cluster is paused. Refusing to reconcile.")
-		return reconcile.Result{}, nil
+		return reconcile.Result{Requeue: true}, nil
 	}
+	return reconcile.Result{}, nil
+}
 
+func (r *CloudStackClusterReconciler) patchCSCluster(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	// Setup patcher. This ensures modifications to the csCluster copy fetched above are patched into the origin.
-	patchHelper, err := patch.NewHelper(csCluster, r.Client)
+	patchHelper, err := patch.NewHelper(r.CC.CSCluster, r.Client)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
-	defer func() {
-		if err = patchHelper.Patch(ctx, csCluster); err != nil {
-			err = errors.Wrapf(err, "error patching CloudStackCluster %s/%s", csCluster.Namespace, csCluster.Name)
-			retErr = multierror.Append(retErr, err)
-		}
-	}()
+	if err = patchHelper.Patch(ctx, r.CC.CSCluster); err != nil {
+		err = errors.Wrapf(err, "error patching CloudStackCluster %s/%s", r.CC.CSCluster.Namespace, r.CC.CSCluster.Name)
+		//retErr = multierror.Append(err, err)
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
 
-	// Delete Cluster Resources if deletion timestamp present.
-	if !csCluster.DeletionTimestamp.IsZero() {
-		return r.reconcileDelete(ctx, log, csCluster)
+// Fetches all CRDs relavent to reconciling a CloudStackCluster.
+func (r *CloudStackClusterReconciler) fetchRelatedCRDs(
+	ctx context.Context, req ctrl.Request) (retRes ctrl.Result, reterr error) {
+
+	r.CC = &ClusterContext{
+		CSCluster:   &infrav1.CloudStackCluster{},
+		CAPICluster: &capiv1.Cluster{},
+		Zones:       &infrav1.CloudStackZoneList{}}
+
+	// Get CloudStack cluster.
+	if reterr = r.Client.Get(ctx, req.NamespacedName, r.CC.CSCluster); reterr != nil {
+		return ctrl.Result{}, errors.Wrap(reterr, "error encountered while fetching CloudStackCluster crd")
+	}
+	// if client.IgnoreNotFound(err) == nil {
+	// 	log.Info("Cluster not found.")
+	// }
+	// return ctrl.Result{}, client.IgnoreNotFound(err)
+
+	// Get CAPI cluster.
+	r.CC.CAPICluster, reterr = util.GetOwnerCluster(ctx, r.Client, r.CC.CSCluster.ObjectMeta)
+	if reterr != nil {
+		return ctrl.Result{}, errors.Wrap(reterr, "error encountered while fetching CAPI Cluster crd")
+	} else if r.CC.CAPICluster == nil {
+		return ctrl.Result{}, errors.New("CAPI Cluster not found")
 	}
 
-	// Reconcile remaining clusters.
-	return r.reconcile(ctx, log, csCluster)
+	// Get CloudStackZones.
+	_, reterr = r.fetchZones(ctx, req)
+	if reterr != nil {
+		return reconcile.Result{}, errors.Wrap(reterr, "error encountered fetching CloudStackZone(s)")
+	}
+	//log.Info("Waiting for CAPI Cluster controller to set owner reference on CloudStack cluster.")
+	return ctrl.Result{}, nil
 }
 
 // Actually reconcile cluster.
 func (r *CloudStackClusterReconciler) reconcile(
 	ctx context.Context,
-	log logr.Logger,
-	csCluster *infrav1.CloudStackCluster,
+	req ctrl.Request,
 ) (ctrl.Result, error) {
 
-	log.V(1).Info("Reconciling CloudStackCluster.", "clusterSpec", csCluster.Spec)
+	// Reconcile only if deletion timestamp not present.
+	if !r.CC.CSCluster.DeletionTimestamp.IsZero() {
+		return ctrl.Result{}, nil
+	}
+
+	r.Log.V(1).Info("Reconciling CloudStackCluster.", "clusterSpec", r.CC.CSCluster.Spec)
+
+	// Generate zones.
+	for _, zone := range r.CC.CSCluster.Spec.Zones {
+		if err := r.generateZone(ctx, r.CC.CSCluster, zone); err != nil {
+			if !strings.Contains(strings.ToLower(err.Error()), "already exists") {
+				return reconcile.Result{}, errors.Wrap(err, "error encountered when creating CloudStackZone")
+			}
+		}
+	}
 
 	// Prevent premature deletion of the csCluster construct from CAPI.
-	controllerutil.AddFinalizer(csCluster, infrav1.ClusterFinalizer)
+	controllerutil.AddFinalizer(r.CC.CSCluster, infrav1.ClusterFinalizer)
 	// Set ready status so that a partial reconcile can be patched.
 	// Ready is required, and patching will fail otherwise.
-	csCluster.Status.Ready = false
+	r.CC.CSCluster.Status.Ready = false
 
 	// Create and or fetch cluster components.
-	err := r.CS.GetOrCreateCluster(csCluster)
+	err := r.CS.GetOrCreateCluster(r.CC.CSCluster)
 	if err == nil {
-		log.Info("Fetched cluster info successfully.")
-		log.V(1).Info("Post fetch cluster status.", "clusterStatus", csCluster.Status)
+		r.Log.Info("Fetched cluster info successfully.")
+		r.Log.V(1).Info("Post fetch cluster status.", "clusterStatus", r.CC.CSCluster.Status)
 
 		// Set cluster to ready to indicate readiness to CAPI.
-		csCluster.Status.Ready = true
+		r.CC.CSCluster.Status.Ready = true
 	}
 
 	return ctrl.Result{}, err
 }
 
 // Delete a cluster.
-func (r *CloudStackClusterReconciler) reconcileDelete(
-	ctx context.Context,
-	log logr.Logger,
-	csCluster *infrav1.CloudStackCluster,
-) (ctrl.Result, error) {
+func (r *CloudStackClusterReconciler) reconcileDelete(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 
-	log.V(1).Info("reconcileDelete CloudStackCluster...")
+	// Reconcile deletion only if deletion timestamp is present.
+	if r.CC.CSCluster.DeletionTimestamp.IsZero() {
+		return ctrl.Result{}, nil
+	}
 
-	if err := r.CS.DisposeClusterResources(csCluster); err != nil {
+	r.Log.V(1).Info("reconcileDelete CloudStackCluster...")
+
+	if err := r.CS.DisposeClusterResources(r.CC.CSCluster); err != nil {
 		return ctrl.Result{}, err
 	}
 
-	controllerutil.RemoveFinalizer(csCluster, infrav1.ClusterFinalizer)
+	controllerutil.RemoveFinalizer(r.CC.CSCluster, infrav1.ClusterFinalizer)
 	return ctrl.Result{}, nil
 }
 
@@ -212,12 +259,15 @@ func (r *CloudStackClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	)
 }
 
-func (r *CloudStackClusterReconciler) generateZone(ctx context.Context, csCluster *infrav1.CloudStackCluster, zoneSpec infrav1.Zone) error {
+func (r *CloudStackClusterReconciler) generateZone(
+	ctx context.Context, csCluster *infrav1.CloudStackCluster, zoneSpec infrav1.Zone,
+) error {
+
 	csZone := &infrav1.CloudStackZone{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      zoneSpec.Name,
-			Namespace: "capc-system", // was kcp.Namespace,
-			// Labels:      internal.ControlPlaneMachineLabelsForCluster(csCluster, csCluster.Name),
+			Name:        strings.ToLower(zoneSpec.Name),
+			Namespace:   csCluster.Namespace,
+			Labels:      map[string]string{"OwnedBy": csCluster.Name},
 			Annotations: map[string]string{},
 			OwnerReferences: []metav1.OwnerReference{
 				*metav1.NewControllerRef(csCluster, controlplanev1.GroupVersion.WithKind("CloudStackCluster")),
@@ -227,7 +277,37 @@ func (r *CloudStackClusterReconciler) generateZone(ctx context.Context, csCluste
 	}
 
 	if err := r.Client.Create(ctx, csZone); err != nil {
-		return errors.Wrap(err, "failed to create machine")
+		return errors.Wrap(err, "failed to create zone")
 	}
 	return nil
+}
+
+// fetchZones fetches CloudStackZones owned by a CloudStackCluster via an ownership label.
+func (r *CloudStackClusterReconciler) fetchZones(
+	ctx context.Context,
+	req ctrl.Request,
+) (ctrl.Result, error) {
+
+	labels := map[string]string{"OwnedBy": r.CC.CSCluster.Name}
+	if err := r.Client.List(
+		ctx,
+		r.CC.Zones,
+		client.InNamespace(r.CC.CSCluster.Namespace),
+		client.MatchingLabels(labels),
+	); err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to list zones")
+	}
+	return ctrl.Result{}, nil
+}
+
+// Run runs all given CloudStackClusterReconcilerFunc, and returns on either the first encountered error, or after all
+// successfully complete.
+func (r *CloudStackClusterReconciler) RunWith(
+	ctx context.Context, req ctrl.Request, fns ...CloudStackClusterReconcilerFunc) (ctrl.Result, error) {
+	for _, fn := range fns {
+		if rslt, err := fn(ctx, req); err != nil || rslt.Requeue == true || rslt.RequeueAfter != 0 {
+			return rslt, err
+		}
+	}
+	return ctrl.Result{}, nil
 }
