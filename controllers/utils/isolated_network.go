72d9a2d979d169665bb3c8237d454ffaf1edada8
Joshua Reed <rejoshed@amazon.com>
2022-05-10 11:36:37 -0700

    Fixed, but not tested.

--- controllers/utils/isolated_network.go
+++ controllers/utils/isolated_network.go
@@ -17,6 +17,7 @@ limitations under the License.
 package utils
 
 import (
+	"fmt"
 	"strings"
 
 	infrav1 "github.com/aws/cluster-api-provider-cloudstack/api/v1beta1"
@@ -27,8 +28,9 @@ import (
 func (r *ReconciliationRunner) GenerateIsolatedNetwork(name string) CloudStackReconcilerMethod {
 	return func() (ctrl.Result, error) {
 		lowerName := strings.ToLower(name)
+		metaName := fmt.Sprintf("%s-%s", r.CSCluster.Name, lowerName)
 		csIsoNet := &infrav1.CloudStackIsolatedNetwork{}
-		csIsoNet.ObjectMeta = r.NewChildObjectMeta(lowerName)
+		csIsoNet.ObjectMeta = r.NewChildObjectMeta(metaName)
 		csIsoNet.Spec.Name = lowerName
 		csIsoNet.Spec.ControlPlaneEndpoint.Host = r.CSCluster.Spec.ControlPlaneEndpoint.Host
 		csIsoNet.Spec.ControlPlaneEndpoint.Port = r.CSCluster.Spec.ControlPlaneEndpoint.Port
