e1e449ee5bc15015a104ffddd93656f8e4a3f573
Wonkun Kim <wonkun@amazon.com>
2022-05-20 09:58:46 -0500

    Add subdomain e2e test

--- test/e2e/subdomain.go
+++ test/e2e/subdomain.go
@@ -31,15 +31,14 @@ import (
 	"sigs.k8s.io/cluster-api/util"
 )
 
-// AffinityGroupSpec implements a test that verifies that an app deployed to the workload cluster works.
-func AffinityGroupSpec(ctx context.Context, inputGetter func() CommonSpecInput) {
+// SubdomainSpec implements a test that verifies that an app deployed to the workload cluster works.
+func SubdomainSpec(ctx context.Context, inputGetter func() CommonSpecInput) {
 	var (
-		specName         = "affinity-group"
+		specName         = "subdomain"
 		input            CommonSpecInput
 		namespace        *corev1.Namespace
 		cancelWatches    context.CancelFunc
 		clusterResources *clusterctl.ApplyClusterTemplateAndWaitResult
-		affinityIds      []string
 	)
 
 	BeforeEach(func() {
@@ -57,49 +56,32 @@ func AffinityGroupSpec(ctx context.Context, inputGetter func() CommonSpecInput) 
 		clusterResources = new(clusterctl.ApplyClusterTemplateAndWaitResult)
 	})
 
-	It("Should have host affinity group when affinity is pro", func() {
-		executeTest(ctx, input, namespace, specName, clusterResources, "pro")
-	})
+	It("Should create a cluster in a subdomain", func() {
+		clusterctl.ApplyClusterTemplateAndWait(ctx, clusterctl.ApplyClusterTemplateAndWaitInput{
+			ClusterProxy:    input.BootstrapClusterProxy,
+			CNIManifestPath: input.E2EConfig.GetVariable(CNIPath),
+			ConfigCluster: clusterctl.ConfigClusterInput{
+				LogFolder:                filepath.Join(input.ArtifactFolder, "clusters", input.BootstrapClusterProxy.GetName()),
+				ClusterctlConfigPath:     input.ClusterctlConfigPath,
+				KubeconfigPath:           input.BootstrapClusterProxy.GetKubeconfigPath(),
+				InfrastructureProvider:   clusterctl.DefaultInfrastructureProvider,
+				Flavor:                   specName,
+				Namespace:                namespace.Name,
+				ClusterName:              fmt.Sprintf("%s-%s", specName, util.RandomString(6)),
+				KubernetesVersion:        input.E2EConfig.GetVariable(KubernetesVersion),
+				ControlPlaneMachineCount: pointer.Int64Ptr(1),
+				WorkerMachineCount:       pointer.Int64Ptr(1),
+			},
+			WaitForClusterIntervals:      input.E2EConfig.GetIntervals(specName, "wait-cluster"),
+			WaitForControlPlaneIntervals: input.E2EConfig.GetIntervals(specName, "wait-control-plane"),
+			WaitForMachineDeployments:    input.E2EConfig.GetIntervals(specName, "wait-worker-nodes"),
+		}, clusterResources)
 
-	It("Should have host affinity group when affinity is anti", func() {
-		executeTest(ctx, input, namespace, specName, clusterResources, "anti")
+		By("PASSED!")
 	})
 
 	AfterEach(func() {
 		// Dumps all the resources in the spec namespace, then cleanups the cluster object and the spec namespace itself.
 		dumpSpecResourcesAndCleanup(ctx, specName, input.BootstrapClusterProxy, input.ArtifactFolder, namespace, cancelWatches, clusterResources.Cluster, input.E2EConfig.GetIntervals, input.SkipCleanup)
-
-		err := CheckAffinityGroupsDeleted(affinityIds)
-		if err != nil {
-			Fail(err.Error())
-		}
 	})
-}
-
-func executeTest(ctx context.Context, input CommonSpecInput, namespace *corev1.Namespace, specName string, clusterResources *clusterctl.ApplyClusterTemplateAndWaitResult, affinityType string) []string {
-	clusterctl.ApplyClusterTemplateAndWait(ctx, clusterctl.ApplyClusterTemplateAndWaitInput{
-		ClusterProxy:    input.BootstrapClusterProxy,
-		CNIManifestPath: input.E2EConfig.GetVariable(CNIPath),
-		ConfigCluster: clusterctl.ConfigClusterInput{
-			LogFolder:                filepath.Join(input.ArtifactFolder, "clusters", input.BootstrapClusterProxy.GetName()),
-			ClusterctlConfigPath:     input.ClusterctlConfigPath,
-			KubeconfigPath:           input.BootstrapClusterProxy.GetKubeconfigPath(),
-			InfrastructureProvider:   clusterctl.DefaultInfrastructureProvider,
-			Flavor:                   "affinity-group-" + affinityType,
-			Namespace:                namespace.Name,
-			ClusterName:              fmt.Sprintf("%s-%s", specName, util.RandomString(6)),
-			KubernetesVersion:        input.E2EConfig.GetVariable(KubernetesVersion),
-			ControlPlaneMachineCount: pointer.Int64Ptr(1),
-			WorkerMachineCount:       pointer.Int64Ptr(1),
-		},
-		WaitForClusterIntervals:      input.E2EConfig.GetIntervals(specName, "wait-cluster"),
-		WaitForControlPlaneIntervals: input.E2EConfig.GetIntervals(specName, "wait-control-plane"),
-		WaitForMachineDeployments:    input.E2EConfig.GetIntervals(specName, "wait-worker-nodes"),
-	}, clusterResources)
-
-	affinityIds := CheckAffinityGroup(clusterResources.Cluster.Name, affinityType)
-
-	By("PASSED!")
-
-	return affinityIds
 }
