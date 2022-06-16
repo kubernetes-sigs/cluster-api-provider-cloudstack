ca656c35bcb68eb147e7ae9f20a729f6c2466906
Joshua Reed <rejoshed@amazon.com>
2022-06-16 11:55:20 -0700

    Updated file. Git knows it was moved and updated now.

--- test/unit/cloud/cloud_suite_test.go
+++ test/unit/cloud/cloud_suite_test.go
@@ -17,13 +17,76 @@ limitations under the License.
 package cloud_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/apache/cloudstack-go/v2/cloudstack"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	"sigs.k8s.io/cluster-api-provider-cloudstack/pkg/cloud"
+	"sigs.k8s.io/cluster-api-provider-cloudstack/test/dummies"
+	"sigs.k8s.io/cluster-api-provider-cloudstack/test/helpers"
+)
+
+var (
+	realCloudClient cloud.Client
+	client          cloud.Client
+	realCSClient    *cloudstack.CloudStackClient
+	testDomainPath  string // Needed in before and in after suite.
 )
 
 func TestCloud(t *testing.T) {
 	RegisterFailHandler(Fail)
+	BeforeSuite(func() {
+		// Create a real cloud client.
+		projDir := os.Getenv("PROJECT_DIR")
+		fmt.Println(projDir)
+		fmt.Println(projDir)
+		fmt.Println(projDir)
+		var connectionErr error
+		realCloudClient, connectionErr = cloud.NewClient(projDir + "/cloud-config")
+		Ω(connectionErr).ShouldNot(HaveOccurred())
+
+		// Create a real CloudStack client.
+		realCSClient, connectionErr = helpers.NewCSClient()
+		Ω(connectionErr).ShouldNot(HaveOccurred())
+
+		// Create a new account and user to run tests that use a real ACS instance.
+		uid := string(uuid.NewUUID())
+		newAccount := cloud.Account{
+			Name:   "TestAccount-" + uid,
+			Domain: cloud.Domain{Name: "TestDomain-" + uid, Path: "ROOT/TestDomain-" + uid}}
+		newUser := cloud.User{Account: newAccount}
+		Ω(helpers.GetOrCreateUserWithKey(realCSClient, &newUser)).Should(Succeed())
+		testDomainPath = newAccount.Domain.Path
+
+		Ω(newUser.APIKey).ShouldNot(BeEmpty())
+
+		// Switch to test account user.
+		cfg := cloud.Config{APIKey: newUser.APIKey, SecretKey: newUser.SecretKey}
+		realCloudClient, connectionErr = realCloudClient.NewClientFromSpec(cfg)
+		Ω(connectionErr).ShouldNot(HaveOccurred())
+	})
+	AfterSuite(func() {
+		if realCSClient != nil { // Check for nil in case the before suite setup failed.
+			// Delete created domain.
+			id, err, found := helpers.GetDomainByPath(realCSClient, testDomainPath)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(found).Should(BeTrue())
+			Ω(helpers.DeleteDomain(realCSClient, id)).Should(Succeed())
+		}
+	})
 	RunSpecs(t, "Cloud Suite")
+}
+
+// FetchIntegTestResources runs through basic CloudStack Client setup methods needed to test others.
+func FetchIntegTestResources() {
+	realCloudClient.ResolveZone(dummies.CSZone1)
+	Ω(dummies.CSZone1.Spec.ID).ShouldNot(BeEmpty())
+	dummies.CSMachine1.Status.ZoneID = dummies.CSZone1.Spec.ID
+	dummies.CSMachine1.Spec.DiskOffering.Name = ""
+	dummies.CSCluster.Spec.ControlPlaneEndpoint.Host = ""
+	Ω(realCloudClient.GetOrCreateIsolatedNetwork(dummies.CSZone1, dummies.CSISONet1, dummies.CSCluster)).Should(Succeed())
 }
